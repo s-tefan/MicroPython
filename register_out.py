93b56ffac471551d33ce1f2a6c1fcf6f3c988ec7
Stefan K <40999112+s-tefan@users.noreply.github.com>
2023-03-14 00:16:24 +0100

    Update register_out.py

--- register_out.py
+++ register_out.py
@@ -4,20 +4,21 @@ import utime
 
 sio_base = const(0xd0000000)
 io_bank0_base = const(0x40014000)
-gpio_oe = const(sio_base + 0x20)
-gpio_out = const(sio_base + 0x10)
+gpio_oe = const(sio_base + 0x20) # output enable (1/0 -> out/in)
+gpio_out = const(sio_base + 0x10) # output value (1/0 -> high/low)
 for k in range(8):
-    machine.mem32[io_bank0_base + k*0x0008 + 0x0004] = 5 # funcsel = 5 sio
+    machine.mem32[io_bank0_base + k*0x08 + 0x04] = 5 # funcsel 5 -> sio på gpio 0--7
 n = 0
-machine.mem32[gpio_oe] |= 0xff # output enable gpio 0-7
+machine.mem32[gpio_oe] |= 0xff # output enable gpio 0--7
 try:
     while True:
         #print(n)
         machine.mem32[gpio_out] = n # set
         utime.sleep_ms(10)
-        n = (n + 1) % 0x100
+        n = (n + 1) & 0xff
 finally:
-    print("Yup!")
+    # Släck när du går, är du snäll ...
+    print("Nu stänger vi!")
     machine.mem32[gpio_out] = 0
     
         
