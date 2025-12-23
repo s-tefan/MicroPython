a036249f0d219ba10bb50b4d63e5a1bc47d0d631
S-tefan <github@k-son.se>
2025-12-23 14:05:16 +0100

    funkis

--- tristate_class.py
+++ tristate_class.py
@@ -1,4 +1,3 @@
-#Not working yet!
 from machine import Pin
 import time
 
@@ -6,21 +5,20 @@ class Tristate:
     HIGH = 1
     LOW = -1
     OFF = 0
-    '''																																																																																																																																																																																																																																																																																																																																																																																																											ån	
-    def __init__(self, state = Tristate.OFF):
+    
+    def __init__(self, state = OFF):
         self.state = state
-    '''
+
 
     def high(self):
-        self.state = Tristate.HIGH
-
+        self.state = self.HIGH
     def low(self):
-        self.state = Tristate.LOW
+        self.state = self.LOW
     def off(self):
-        self.state = Tristate.OFF
+        self.state = self.OFF
 
     def to_pin(self, pin):
-        if self.state == Tristate.HIGH:
+        if self.state == self.HIGH:
             pin.init(mode = Pin.OUT, value = 1 )     
         elif self.state == Tristate.LOW:
             pin.init(mode = Pin.OUT, value = 0 )
@@ -31,26 +29,48 @@ class Tristate:
 
 
 
-def light(n,k,m):
-    return tuple(Tristate.HIGH if i == k \
+def get_states(n,k,m):
+    ''' return n Tristates where #k is HIGH and #m is LOW,
+    otherwise OFF'''
+    return tuple(Tristate(s) for s in tuple(Tristate.HIGH if i == k \
                  else Tristate.LOW if i == m \
-                 else Tristate.OFF for i in range(n))
-                 
+                 else Tristate.OFF for i in range(n)))
 
-def stateset(pinlist, state):
-    for k, pin in enumerate(pinlist):
-        triset(pin, state[k])
-        
-statelist = [light(3,k,m)  for m in range(3) for k in range(m)]
+def activate_pair(pinlist,k,m):
+    ''' activate #k HIGH #m LOW in given list of Pin'''
+    for i, pin in enumerate(pinlist):
+        if i == k:
+            Tristate(Tristate.HIGH).to_pin(pin)
+        elif i == m:
+            Tristate(Tristate.LOW).to_pin(pin)
+        else:
+            Tristate(Tristate.OFF).to_pin(pin)
+
+def activate_states(pinlist,states):
+    ''' activate list of Pin according to states in given '''
+    for i, pin in enumerate(pinlist):
+        states[i].to_pin(pin)
+
+statelist = []
+for m in range(3):
+    for k in range(m):
+        statelist += [get_states(3,m,k),get_states(3,k,m)]
+print(statelist)
 #statelist = [(0,1,2),(1,0,2),(0,2,1),(1,2,0),(2,0,1),(2,1,0)]
 L0 = Pin(0,Pin.IN)
 L1 = Pin(1,Pin.IN)
 L2 = Pin(2,Pin.IN)
 pinlist = [L0,L1,L2]
 for _ in range(1):
-    for state in statelist + list(reversed(statelist)):
-        for pin in pinlist:
-            state.to_pin(pin)
+    for states in statelist + list(reversed(statelist)):
+        print(*(ts.state for ts in states))
+        activate_states(pinlist, states)
         time.sleep(0.1)
+time.sleep(2)
         
-
+for m in range(3):
+    for k in range(m):
+        activate_pair(pinlist,m,k)
+        time.sleep(1)
+        activate_pair(pinlist,k,m)
+        time.sleep(1)
